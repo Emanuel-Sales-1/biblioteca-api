f821090d3af7957a153c5f970482610441171ed9
Emanuel <emanuel.sales.060@gmail.com>
2026-09-10 22:43:12 -0300

    feat: add book_author SQLAlchemy model

--- app/models/book.py
+++ app/models/book.py
@@ -1,4 +1,5 @@
 from app.database import Base
+from sqlalchemy import Table, Column, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -9,3 +10,10 @@ class Book(Base):
     title: Mapped[str]
     publication_year: Mapped[int]
     publisher: Mapped[str]
+
+book_author = Table(
+    "book_author",
+    Base.metadata,
+    Column("author_id", ForeignKey("author.id"), primary_key=True),
+    Column("book_id", ForeignKey("book.id"), primary_key=True)
+)
